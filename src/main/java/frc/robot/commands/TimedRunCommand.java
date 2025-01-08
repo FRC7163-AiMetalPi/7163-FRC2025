e6d7cbcba73f89d7d7ba24fb28935172efd48cb1
Neel Sharma <nsdj.sharma@gmail.com>
2025-01-08 16:51:15 +1100

    make timedruncommand *actually* parallel

--- src/main/java/frc/robot/commands/TimedRunCommand.java
+++ src/main/java/frc/robot/commands/TimedRunCommand.java
@@ -1,14 +1,14 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 /**
  * A command that runs a function repeatedly for the specified duration.
  */
-public class TimedRunCommand extends SequentialCommandGroup {
+public class TimedRunCommand extends ParallelCommandGroup {
   /**
    * @param toRun        The runnable to execute repeatedly
    * @param duration     The duration of the command
